216e8a473d181668e7c7ed1cb70a662b83eec447
aimeerpierce <aimeerpierce@gmail.com>
2016-11-15 17:23:04 -0600

    library generated and display for librarian done

--- app.js
+++ app.js
@@ -20,39 +20,39 @@ app.service('library',function(){
 		user: "",
 		librarian: false,
 		books:  [{"Art": [
-					{id: 1, title: "Management of Art Galleries", type: "art", presence: 1, borrowedBy: " "},
-					{id: 2, title: "Paint the Revolution: Mexican Modernism", type: "art", presence: 1, borrowedBy: " "},
-					{id: 3, title: "Monet: The Early Years", type: "art", presence: 1, borrowedBy: " "},
-					{id: 4, title: "Abstract Expressionism", type: "art", presence: 1, borrowedBy: " "},
-					{id: 5, title: "Beyond Caravaggio", type: "art", presence: 1, borrowedBy: " "},
+					{id: 1, title: "Management of Art Galleries", type: "art", presence: 1, borrowedBy: "n/a"},
+					{id: 2, title: "Paint the Revolution: Mexican Modernism", type: "art", presence: 1, borrowedBy: "n/a"},
+					{id: 3, title: "Monet: The Early Years", type: "art", presence: 1, borrowedBy: "n/a"},
+					{id: 4, title: "Abstract Expressionism", type: "art", presence: 1, borrowedBy: "n/a"},
+					{id: 5, title: "Beyond Caravaggio", type: "art", presence: 1, borrowedBy: "n/a"},
 				]},
 				{"Science" : [
-					{id: 6, title: "A Brief History ofTime" , type: "science", presence: 1, borrowedBy: " "},
-					{id: 7, title: "The Selfish Gene", type: "science", presence: 1, borrowedBy: " "},
-					{id: 8, title: "A Short History of Nearly Everything", type: "science", presence: 1, borrowedBy: " "},
-					{id: 9, title: "Rise of the Rocket Girls", type: "science", presence: 1, borrowedBy: " "},
-					{id: 10, title: "Euclid's Elements", type: "science", presence: 1, borrowedBy: " "},
+					{id: 6, title: "A Brief History ofTime" , type: "science", presence: 1, borrowedBy: "n/a"},
+					{id: 7, title: "The Selfish Gene", type: "science", presence: 1, borrowedBy: "n/a"},
+					{id: 8, title: "A Short History of Nearly Everything", type: "science", presence: 1, borrowedBy: "n/a"},
+					{id: 9, title: "Rise of the Rocket Girls", type: "science", presence: 1, borrowedBy: "n/a"},
+					{id: 10, title: "Euclid's Elements", type: "science", presence: 1, borrowedBy: "n/a"},
 				]},
 				{"Sport": [
-					{id: 11, title: "Alone on the Wall", type: "sport", presence: 1, borrowedBy: " "},
-					{id: 12, title: "Mountaineering: Freedom of the Hills", type: "sport", presence: 1, borrowedBy: " "},
-					{id: 13, title: "Vertical Mind", type: "sport", presence: 1, borrowedBy: " "},
-					{id: 14, title: "Girl on the Rocks", type: "sport", presence: 1, borrowedBy: " "},
-					{id: 15, title: "Climbing Free", type: "sport", presence: 1, borrowedBy: " "},
+					{id: 11, title: "Alone on the Wall", type: "sport", presence: 1, borrowedBy: "n/a"},
+					{id: 12, title: "Mountaineering: Freedom of the Hills", type: "sport", presence: 1, borrowedBy: "n/a"},
+					{id: 13, title: "Vertical Mind", type: "sport", presence: 1, borrowedBy: "n/a"},
+					{id: 14, title: "Girl on the Rocks", type: "sport", presence: 1, borrowedBy: "n/a"},
+					{id: 15, title: "Climbing Free", type: "sport", presence: 1, borrowedBy: "n/a"},
 				]},
 				{"Literature": [
-					{id: 16, title: "Pride and Prejudice", type: "literature", presence: 1, borrowedBy: " "},
-					{id: 17, title: "Sense and Sensibility", type: "literature", presence: 1, borrowedBy: " "},
-					{id: 18, title: "The Wise Man's Fear", type: "literature", presence: 1, borrowedBy: " "},
-					{id: 19, title: "1984", type: "literature", presence: 1, borrowedBy: " "},
-					{id: 20, title: "Fahrenheit 451", type: "literature", presence: 1, borrowedBy: " "},
+					{id: 16, title: "Pride and Prejudice", type: "literature", presence: 1, borrowedBy: "n/a"},
+					{id: 17, title: "Sense and Sensibility", type: "literature", presence: 1, borrowedBy: "n/a"},
+					{id: 18, title: "The Wise Man's Fear", type: "literature", presence: 1, borrowedBy: "n/a"},
+					{id: 19, title: "1984", type: "literature", presence: 1, borrowedBy: "n/a"},
+					{id: 20, title: "Fahrenheit 451", type: "literature", presence: 1, borrowedBy: "n/a"},
 				]},
 				{"Reference": [
-					{id: 1, title: "Reference Book 1", type: "reference", presence: 1, borrowedBy: " "},
-					{id: 2, title: "Reference Book 2", type: "reference", presence: 1, borrowedBy: " "},
-					{id: 3, title: "Reference Book 3", type: "reference", presence: 1, borrowedBy: " "},
-					{id: 4, title: "Reference Book 4", type: "reference", presence: 1, borrowedBy: " "},
-					{id: 5, title: "Reference Book 5", type: "reference", presence: 1, borrowedBy: " "},
+					{id: 1, title: "Reference Book 1", type: "reference", presence: 1, borrowedBy: "n/a"},
+					{id: 2, title: "Reference Book 2", type: "reference", presence: 1, borrowedBy: "n/a"},
+					{id: 3, title: "Reference Book 3", type: "reference", presence: 1, borrowedBy: "n/a"},
+					{id: 4, title: "Reference Book 4", type: "reference", presence: 1, borrowedBy: "n/a"},
+					{id: 5, title: "Reference Book 5", type: "reference", presence: 1, borrowedBy: "n/a"},
 				]}]
 		};
 
@@ -80,15 +80,19 @@ app.controller('loginController', ['$scope','$location','library',function($scop
 app.controller('librarianController', ['$scope','library', function($scope,library) {
 	$scope.name = "librarianView";
 	$scope.library = library;
-	$scope.populateBooks = function(library){
-
-	}
+	$scope.displayInfo = function(book){
+		var status = " ";
+		if(book.presence == 1){
+			status = "Present";
+		} else {
+			status = "Checked Out";
+		}
+		alert("Title: "+book.title+ "; Type: "+book.type+"; Status: "+status+"; Borrowed By: "+book.borrowedBy+";");
+	};
 
 	$scope.addBook = function(book){
 		//book.(title)(shelf)(ordinary)(reference)
-
-
-	}
+	};
 }]);
 
 app.controller('studentController', ['$scope','library',function($scope,library) {
